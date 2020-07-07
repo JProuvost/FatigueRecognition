086b7c641e7a8966f39c693c03a59ce3e9d0149c
Siemon <alexandre.lambert@edu.ece.fr>
2020-07-07 15:59:05 +0200

    fix bug and add delete_frame_method but not working

--- exporting_data_for_ontho.py
+++ exporting_data_for_ontho.py
@@ -14,10 +14,10 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from skimage.feature import hog
 import joblib
 
-os.chdir("/Users/Jean/Desktop/data_for_ontho")
+os.chdir(os.getcwd())
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-  
+
 LBFmodel_url= "https://github.com/kurnianggoro/GSOC2017/raw/master/data/lbfmodel.yaml"
 
 LBFmodel = "lbfmodel.yaml"
@@ -28,7 +28,7 @@ class HogTransformer(BaseEstimator, TransformerMixin):
     Expects an array of 2d arrays (1 channel images)
     Calculates hog features for each img
     """
- 
+
     def __init__(self, y=None, orientations=9,
                  pixels_per_cell=(8, 8),
                  cells_per_block=(3, 3), block_norm='L2-Hys'):
@@ -37,25 +37,25 @@ class HogTransformer(BaseEstimator, TransformerMixin):
         self.pixels_per_cell = pixels_per_cell
         self.cells_per_block = cells_per_block
         self.block_norm = block_norm
- 
+
     def fit(self, X, y=None):
         return self
- 
+
     def transform(self, X, y=None):
- 
+
         def local_hog(X):
             return hog(X,
                        orientations=self.orientations,
                        pixels_per_cell=self.pixels_per_cell,
                        cells_per_block=self.cells_per_block,
                        block_norm=self.block_norm)
- 
+
         try: # parallel
             return np.array([local_hog(img) for img in X])
         except:
             return np.array([local_hog(img) for img in X])
 
-          
+
 hogify = HogTransformer(
     pixels_per_cell=(8, 8),
     cells_per_block=(2,2),
@@ -64,7 +64,7 @@ hogify = HogTransformer(
 )
 
 
-sgd_clf = joblib.load('svm_for_eyes.joblib') 
+sgd_clf = joblib.load("svm_for_eyes.joblib")
 
 def frame_extraction_v():
     print("ui3")
@@ -72,10 +72,10 @@ def frame_extraction_v():
     i=0
     while(cap.isOpened()):
         ret, frame = cap.read()
-        cv2.imshow('frame', frame) 
+        cv2.imshow('frame', frame)
         if ret == False:
             break
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         cv2.imwrite('frame'+str(i)+'.jpg',frame)
         i+=1
@@ -83,7 +83,16 @@ def frame_extraction_v():
     cv2.destroyAllWindows()
     frame_nb=i
     return frame_nb
-  
+
+def delete_frame_file(a):
+    i=0
+    while (i<a):
+        print("here")
+        print(a)
+        print(os.path.exists("frame"+str(i)+".jpg"))
+        if os.path.exists("frame"+str(i)+".jpg") :
+            os.remove("frame"+str(i)+".jpg")
+
 
 def data_obtention(frame_nb):
     print("ui2")
@@ -126,11 +135,11 @@ def data_obtention(frame_nb):
                 eye_2_ear=(eye_2_v_1+eye_2_v_2)/2*eye_2_h
                 mouth_openess=np.linalg.norm(landmark[0][52]-landmark[0][63])
                 print("ui13")
-                img_eye_left= img[(landmark[0][42][1]-5):landmark[0][20][1] , landmark[0][18][0]:landmark[0][22][0]].copy()
-                img_eye_right= img[(landmark[0][48][1]-5):landmark[0][20][1] , landmark[0][18][0]:landmark[0][22][0]].copy()
+                img_eye_left= gray[int(landmark[0][20][1]):int(landmark[0][42][1]-5) , int(landmark[0][18][0]):int(landmark[0][22][0])].copy()
+                img_eye_right= gray[int(landmark[0][48][1]-5):int(landmark[0][20][1]) , int(landmark[0][18][0]):int(landmark[0][22][0])].copy()
                 print("ui14")
-                img_eye_left=img_eye_left.resize((1,54,54))
-                img_eye_right=img_eye_right.resize((1,54,54))
+                img_eye_left=np.resize(img_eye_left,(1,54,54))
+                img_eye_right=np.resize(img_eye_left,(1,54,54))
                 print("ui15")
                 img_eye_left_hog=hogify.fit_transform(img_eye_left)
                 img_eye_right_hog=hogify.fit_transform(img_eye_right)
@@ -141,10 +150,11 @@ def data_obtention(frame_nb):
                 facial_measures[i]=[eye_1_ear,eye_2_ear,mouth_openess,left_eye_predict,right_eye_predict]
         i+=1
     return facial_measures
-  
+
 print("ui")
-  
+
 a=frame_extraction_v()
 print(a)
 facial_measures=data_obtention(a)
 np.savetxt('data.csv', facial_measures, delimiter=',')
+delete_frame_file(a)
